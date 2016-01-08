66b4e64a304484d392d966673f280575e6f7af0f
GMeyr <gabrielmeyr@gmail.com>
2016-01-08 16:24:13 -0500

    resetting files b/c of crashed server

--- client/home/home.js
+++
@@ -1,8 +0,0 @@
-angular.module('jaguarTask.home', [])
-
-.controller('HomePageController', function($scope, $location){
-  $scope.routeToMain = function(){
-    console.log("routeToMain click");
-    //$location.path('main');
-    };
-});
